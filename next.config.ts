9dc5f0fce46573228b0e3d1f3b9b1d45b0a3225a
Anant Wagh <anantwagh24@gmail.com>
2026-08-31 19:12:41 +0530

    fix(tunnel): whitelist pinggy domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,8 @@ const securityHeaders = [
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: [
+    "*.pinggy.net",
+    "*.pinggy-free.link",
     "9b1c8ecad53185.lhr.life",
     "*.lhr.life",
     "upset-cobras-draw.loca.lt",
